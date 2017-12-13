83be4f427b6712cc3d67e357d4b032dfe20243f5
Рогов Никита <Nikita1998_2010@mail.ru>
2017-12-13 19:18:40 +0300

    Исправлен первый коммит

---
+++ SciBuy.Domain/Infrastructure/UserManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SciBuy.Domain
+{
+    public class UserManager
+    {
+        public void Create()
+        { }
+        public void Edit()
+        { }
+        public void Delete()
+        { }
+    }
+}
