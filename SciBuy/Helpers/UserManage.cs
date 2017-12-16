8c1748de9adb516160081a7b6921c1c82c024fe6
Рогов Никита <Nikita1998_2010@mail.ru>
2017-12-16 20:59:24 +0300

    Создание пользователя

---
+++ SciBuy/Helpers/UserManage.cs
@@ -0,0 +1,20 @@
+﻿using System.Web;
+using System.Web.Mvc;
+using SciBuy.Infrastructure;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.AspNet.Identity;
+using SciBuy.Models;
+using System.Threading.Tasks;
+
+
+namespace SciBuy.Helpers
+{
+    public class UserManage
+    {
+        public void Create(AppUser user)
+        {
+
+        }
+
+    }
+}
--- SciBuy/Infrastructure/AppIdentityDbContext.cs
+++ SciBuy/Infrastructure/AppIdentityDbContext.cs
@@ -6,14 +6,14 @@ using Microsoft.AspNet.Identity;
 namespace SciBuy.Infrastructure
 {
     public class AppIdentityDbContext : IdentityDbContext<AppUser>
-    {
+    {   
         public AppIdentityDbContext() : base("name=IdentityDb") { }
 
         static AppIdentityDbContext()
         {
             Database.SetInitializer<AppIdentityDbContext>(new IdentityDbInit());
         }
-
+        public DbSet<MetaField> User_Meta { get; set; }
         public static AppIdentityDbContext Create()
         {
             return new AppIdentityDbContext();
@@ -21,5 +21,7 @@ namespace SciBuy.Infrastructure
     }
 
     public class IdentityDbInit : NullDatabaseInitializer<AppIdentityDbContext>
-    { }
+    {
+
+    }
 }
